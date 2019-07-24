2c317d7ebb5ecb4ee53ebf17473b9d200355aed2
YYcoder <2082878884@qq.com>
2019-07-24 15:45:08 +0800

    '删除了wxapp文件夹'

---
+++ controller_yycoder/src/main/java/com/zczp/controller_yycoder/HomeController.java
@@ -0,0 +1,12 @@
+package com.zczp.controller_yycoder;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Api(tags = "主页信息模块")
+@RequestMapping("home")
+public class HomeController {
+}
---
+++ controller_yycoder/src/main/java/com/zczp/controller_yycoder/UserController.java
@@ -0,0 +1,13 @@
+package com.zczp.controller_yycoder;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+@Api(tags = "个人信息模块")
+public class UserController {
+
+
+}
