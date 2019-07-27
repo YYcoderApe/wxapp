cae0f6a03615c999bc422b04d9b56730452fa2f0
cxy <chenxuyang.win@qq.com>
2019-07-27 14:18:38 +0800

    提交

--- controller_cancer/src/main/java/com/zczp/controller_cancer/PushController.java
+++ controller_cancer/src/main/java/com/zczp/controller_cancer/PushController.java
@@ -1,9 +1,39 @@
 package com.zczp.controller_cancer;
 
+import com.zczp.util.AjaxResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "发布招聘信息模块")
+@RequestMapping("/api/pushPost")
 public class PushController {
+
+    @ApiOperation("发布招聘岗位")
+    @PostMapping("/pushPost")
+    public AjaxResult pushPost(){
+        return null;
+    }
+
+    @ApiOperation("开放城市")
+    @GetMapping("/city")
+    public AjaxResult searchCity(){
+        return null;
+    }
+
+    @ApiOperation("搜索公司")
+    @GetMapping("/searchCompany")
+    public AjaxResult searchCompany(){
+        return null;
+    }
+
+    @ApiOperation("展示搜索前12名")
+    @GetMapping("/showCompany")
+    public AjaxResult showCompany(){
+        return null;
+    }
 }
--- service_yycoder/src/main/java/com/zczp/service_yycoder/HomeService.java
+++ service_yycoder/src/main/java/com/zczp/service_yycoder/HomeService.java
@@ -3,25 +3,12 @@ package com.zczp.service_yycoder;
 
 import com.zczp.entity.TbPost;
 import com.zczp.entity.TbPostWithBLOBs;
-import com.zczp.vo_yycoder.PostDetailVo;
+
 
 import java.util.List;
 
 public interface HomeService {
 
-    //查询post中所需要的字段打包给前端
-    List<PostDetailVo> getPostDetail();
 
-    //查询post表对应的详情TbPostWithBLOBs
-    TbPostWithBLOBs selectByPostId(Integer postId);
-
-    //根据城市cityName 进行字段查询
-    List<PostDetailVo> getPostByCityName(String cityName);
-
-    //根据招聘类型jobType就行查询
-    List<PostDetailVo> getPostByJobType(String jobType);
-
-    //根据岗位类型typeId进行查询
-    List<PostDetailVo> getPostByPostType(String postType);
 
 }
